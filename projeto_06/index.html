2c85a8741ba03a43d0b7f86d842879abc4a44b71
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-03-24 13:33:37 -0300

    Finalizando Projeto 6 e 7

---
+++ projeto_06/index.html
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet">
+	<title>Projeto 06</title>
+</head>
+<body>
+	<div class="icons-social">
+		<a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	</div><!--icons-social-->
+	<section class="main">
+		<div class="center">
+			<header>
+				<div class="logo">
+					Danki.Code
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div><!--center-->
+	</section><!--main-->
+</body>
+</html>
---
+++ projeto_06/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet">
+	<title>Projeto 06</title>
+</head>
+<body>
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				</div><!--icons-social-->
+				<div class="clear"></div>
+				<div class="logo">
+					Danki.Code
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+				<nav class="mobile">
+					<i class="fa fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>	
+				</nav>
+				<div class="clear"></div>
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao nosso <span>website!</span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit i</p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit i</p>
+				<a href="">Saiba mais!</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+	</section><!--main-->
+
+	<section class="planos">
+		<div class="chamada-planos">
+		<div class="center">
+			<h2>Confira nossos planos</h2>
+		</div>	
+		</div><!--chamada-planos-->
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+					</div>
+				</div><!--box-planos-single-->
+			
+			<div class="box-planos-single">
+			<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+				</div>
+			</div><!--box-planos-single-->
+
+			<div class="box-planos-single">
+			<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+						<p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+					</div>
+				</div><!--box-planos-single-->
+
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section>
+
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+		</div>
+	</section>
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div>Entre em contato!</h2>
+				<form>
+					<input type="text" placeholder="Insira seu nome...">
+					<input type="email" placeholder="Insira seu e-mail...">
+					<textarea placeholder="Sua mensagem..."></textarea>
+					<input type="submit" value="Enviar!">
+				</form>
+			</div>
+
+			<div class="w50 time">
+				<h2><div class="borda-efeito"></div>Conheça nosso time!</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Guilherme Grillo</p>
+							<p style="font-weight: italic;">CEO / Instrutor</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+				
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Guilherme Grillo</p>
+							<p style="font-weight: italic;">CEO / Instrutor</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Guilherme Grillo</p>
+							<p style="font-weight: italic;">CEO / Instrutor</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Guilherme Grillo</p>
+							<p style="font-weight: italic;">CEO / Instrutor</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+	<footer>
+		<p>Todos os direitos reservados ao Projeto #6!</p>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script>
+		$(function(){
+			$('nav.mobile i').click(function(){
+				var el = $(this).parent().find('ul');
+				if(el.is(':visible') == false){
+				 el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			})
+		})
+	</script>
+</body>
+</html>
