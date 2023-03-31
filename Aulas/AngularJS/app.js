807ae2260c9615d28e11964dfb80106118fb89ff
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-03-31 14:45:05 -0300

    Exemplo utilizando AngularJS

---
+++ Aulas/AngularJS/app.js
@@ -0,0 +1,16 @@
+var app = angular.module("myShoppingList", []);
+app.controller("myCtrl", function($scope) {
+  $scope.products = ["Leite", "Pão", "Queijo"];
+  $scope.addItem = function(){
+    if ($scope.products.indexOf($scope.item) == -1) {
+      $scope.products.push($scope.item);
+    } else {
+      $scope.errortext = "The item is already in your shopping list.";
+    }
+
+  }
+
+  $scope.removeItem = function(x){
+    $scope.products.splice(x,1);
+  }
+});
