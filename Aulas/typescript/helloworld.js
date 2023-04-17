adc1ac589f21f36491b5f326627b4be23b6afe3f
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-04-17 16:06:11 -0300

    Typescript

---
+++ Aulas/typescript/helloworld.js
@@ -0,0 +1,7 @@
+//tipagem de variáveis, podendo utilizar "any" para qualquer uma variável//
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome) {
+        this.nome = nome;
+    }
+    return Pessoa;
+}());
---
+++ Aulas/typescript/helloworld.ts
@@ -0,0 +1,10 @@
+//tipagem de variáveis, podendo utilizar "any" para qualquer uma variável//
+
+class Pessoa{
+
+  nome:string
+
+  constructor(nome:string){
+    this.nome = nome;
+  }
+}
